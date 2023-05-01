85d37bf38ab39db5c88f4aa111fb54e5c6ab3357
Francisco <francisco.carvalho.q@gmail.com>
2023-05-01 18:07:45 -0300

    Ajustes

---
+++ scripts/cadastro.js
@@ -0,0 +1,18 @@
+const visibilidadeSenhaBtn = document.querySelector(".visibilidade-senha-btn")
+
+visibilidadeSenhaBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const inputSenha = document.querySelector("#input-senha")
+    const span = visibilidadeSenhaBtn.getElementsByTagName("span")[0]
+    const visible = span.innerText === "visibility"
+
+    if (visible) {
+        inputSenha.type = "password"
+        span.innerText = "visibility_off"
+    } else {
+        inputSenha.type = "text"
+        span.innerText = "visibility"
+    }
+
+    inputSenha.focus()
+})
